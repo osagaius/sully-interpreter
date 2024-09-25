a71ace01b0aff5eee80527b8670824da5da32bfe
Osayame Gaius-Obaseki <osayame@gmail.coml>
2024-09-25 12:56:28 -0700

    fix heroku serving.

--- backend/app.ts
+++ backend/app.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import router from './router';
+import path from 'path';
 
 const app = express();
 
@@ -9,4 +10,14 @@ app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 
 app.use('/api', router);
 
+
+// Serve static files from the dist directory
+app.use(express.static(path.join(__dirname, '../frontend/dist')));
+
+// Fallback for all other routes to serve index.html
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
+});
+
 export default app;
+
