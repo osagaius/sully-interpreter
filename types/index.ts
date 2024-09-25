f4e1a52ef7401597d0155981d8e130d006e77d2b
Osayame Gaius-Obaseki <osayame@gmail.coml>
2024-09-25 12:13:13 -0700

    fix config.

--- types/index.ts
+++ types/index.ts
@@ -3,6 +3,4 @@ export type RequireSubset<T, K extends keyof T> = T & { [P in K]-?: T[P] }
 
 export type PartialSubset<T, K extends keyof T> = Omit<T, K> & { [P in K]?: T[P] }
 
-export * from './shipments';
-export * from './stops';
 export * from './conversations';
